6368bef055481e6e60d84cf9151fc63a7f55c6e1
Hayoung <shalom99904@gmail.com>
2024-11-25 10:24:38 +0900

    init : 프로젝트 생성

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>OSS Team Project</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
