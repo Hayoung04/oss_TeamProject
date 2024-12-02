30d708ebe996164d5222c2995709cd1b3a1d66c3
parkhasol <hasol1229@handong.ac.kr>
2024-12-02 23:02:37 +0900

    edit : 약간의 코드 수정

--- src/Pages/MockListPage.js
+++ src/Pages/MockListPage.js
@@ -1,12 +1,40 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-const mockListPage = () => {
+const ShowList = () => {
+  const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    fetch("https://674853885801f51535905794.mockapi.io/books")
+      .then((response) => response.json())
+      .then((data) => setBooks(data))
+      .catch((error) => console.error("Error fetching books:", error));
+  }, []);
+
+  return <MockListPage books={books} />;
+};
+
+const MockListPage = ({ books }) => {
+  if (!books || books.length === 0) {
+    return <p>No books available.</p>;
+  }
+
   return (
     <div style={{ textAlign: "center", marginTop: "50px" }}>
-      <h1>Welcome to the mockListPage</h1>
-      <p>This is the main page of the application.</p>
+      <h1>Book List</h1>
+      <ul style={{ listStyle: "none", padding: 0 }}>
+        {books.map((book) => (
+          <li key={book.id} style={{ marginBottom: "30px" }}>
+            <img src={book.image} alt={book.title} width="100" />
+            <p>{book.num}. {book.title}</p>
+            <p>저자 : {book.author}</p>
+            <p>출판사 : {book.publisher}</p>
+            <p>발행년도 : {book.publicationDate}</p>
+            <p>한줄평 : {book.recommendation}</p>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
 
-export default mockListPage;
+export default ShowList;
