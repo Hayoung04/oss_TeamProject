d843b91c5d2a91b5fc1b9fa2da78c8a4e7a473f8
jonghyekim <jonghye.kim@handong.ac.kr>
2024-12-03 18:51:10 +0900

    add: MockAddPage.js

--- src/Pages/MockAddPage.js
+++ src/Pages/MockAddPage.js
@@ -1,40 +1,97 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
+import './MockAddPage.css';
 
-const ShowList = () => {
-  const [books, setBooks] = useState([]);
+const MockAddPage = () => {
+  const [book, setBook] = useState({
+    title: "",
+    author: "",
+    publisher: "",
+    publicationDate: "",
+    image: "",
+    callNumber: "",
+    classificationCriteria: "",
+    recommendation: "",
+  });
 
-  useEffect(() => {
-    fetch("https://674853885801f51535905794.mockapi.io/books")
-      .then((response) => response.json())
-      .then((data) => setBooks(data))
-      .catch((error) => console.error("Error fetching books:", error));
-  }, []);
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setBook({ ...book, [name]: value });
+  };
 
-  return <MockListPage books={books} />;
-};
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
-const MockListPage = ({ books }) => {
-  if (!books || books.length === 0) {
-    return <p>No books available.</p>;
-  }
+    const {
+      title,
+      author,
+      publisher,
+      publicationDate,
+    } = book;
+
+    if (!title || !author || !publisher || !publicationDate) {
+      alert("제목, 저자, 출판사 그리고 발행년도는 필수 항목입니다. 모든 항목을 작성해 주세요.");
+      return;
+    }
+
+    fetch("https://674853885801f51535905794.mockapi.io/books", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(book),
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("오류가 발생하였습니다. 다시한번 입력해주세요.");
+        }
+        return response.json();
+      })
+      .then(() => {
+        alert("도서가 정상적으로 추가되었습니다!");
+        window.location.href = "/";
+      })
+      .catch((err) => alert(err.message));
+  };
 
   return (
-    <div style={{ textAlign: "center", marginTop: "50px" }}>
-      <h1>Book List</h1>
-      <ul style={{ listStyle: "none", padding: 0 }}>
-        {books.map((book) => (
-          <li key={book.id} style={{ marginBottom: "30px" }}>
-            <img src={book.image} alt={book.title} width="100" />
-            <p>{book.num}. {book.title}</p>
-            <p>저자 : {book.author}</p>
-            <p>출판사 : {book.publisher}</p>
-            <p>발행년도 : {book.publicationDate}</p>
-            <p>한줄평 : {book.recommendation}</p>
-          </li>
-        ))}
-      </ul>
+    <div>
+      <h1>도서 추가</h1>
+      <form onSubmit={handleSubmit}>
+
+        <label>제목</label><br />
+        <input type="text" name="title" value={book.title} onChange={handleChange} /><br /><br />
+
+        <label>저자</label><br />
+        <input type="text" name="author" value={book.author} onChange={handleChange} /><br /><br />
+
+        <label>출판사</label><br />
+        <input type="text" name="publisher" value={book.publisher} onChange={handleChange} /><br /><br />
+
+        <label>발행년도</label><br />
+        <input type="text" name="publicationDate" value={book.publicationDate} onChange={handleChange} /><br /><br />
+
+        <label>사진 URL</label><br />
+        <input type="text" name="image" value={book.image} onChange={handleChange} /><br /><br />
+
+        <label>청구기호</label><br />
+        <input type="text" name="callNumber" value={book.callNumber} onChange={handleChange} /><br /><br />
+
+        <label>분류 기준</label><br />
+        <input type="text" name="classificationCriteria" value={book.classificationCriteria} onChange={handleChange} /><br /><br />
+
+        <label>추천 글</label><br />
+        <textarea name="recommendation" value={book.recommendation} onChange={handleChange} /><br /><br />
+
+        <button type="submit">추가</button>
+        <button>
+          <a href="/">
+            취소
+          </a>
+        </button>
+
+      </form>
     </div>
   );
 };
 
-export default ShowList;
+export default MockAddPage;
